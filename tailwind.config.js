72ca2cb3e6f26a7f7700d0d52775cb1ced16058d
Hector Pellegrino <fabropelegrinih@gmail.com>
2024-03-23 21:24:44 -0300

    Instalação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'orange': '#ed8525',
+      'green': '#006971',
+      'dark-green': '#004148',
+      'off-white': 'rgba(255, 255, 255, 0.75)',
+      'white': '#fff',
+      'offwhitebg': '#f2f2f2'
+    }
+  },
+  plugins: [],
+}
